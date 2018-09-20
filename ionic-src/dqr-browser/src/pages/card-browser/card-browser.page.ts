3c4a5c931175c7ce4885fd2a2dc8c29cba26d3cd
Josh Beard <josh.beard@neurocogtrials.com>
2018-09-20 12:53:15 -0400

    Restored basic card brower functionality

--- ionic-src/dqr-browser/src/pages/card-browser/card-browser.page.ts
+++ ionic-src/dqr-browser/src/pages/card-browser/card-browser.page.ts
@@ -10,6 +10,9 @@ import { DQRCard } from "../../core/models";
 export class CardBrowserPage extends Base.DQRBasePage {
 
     public static PAGE_NAME: string = "Card Browser";
+    public showImages: boolean = true;
+
+    public cardMasterList: DQRCard[] = [];
     public cards: DQRCard[] = [];
 
     public get pageTitle(): string {
@@ -21,7 +24,37 @@ export class CardBrowserPage extends Base.DQRBasePage {
         super();
 
         this.cardsService.getCards().subscribe((cards: DQRCard[]) => {
+            this.cardMasterList = [...cards];
             this.cards = cards;
         })
+    }
+
+    public getImagePath(card: DQRCard): string {
+        return this.cardsService.getImagePath(card);
+    }
+
+    public onFilterCards(event: any): void {
+
+        this.cards = [...this.cardMasterList];
+
+        let searchValue: string = event.target.value;
+
+        if (!searchValue || searchValue.trim() === "") {
+            this.onResetFilter();
+            return;
+        }
+
+        searchValue = searchValue.toLowerCase();
+
+        this.cards = this.cards.filter((card: DQRCard) => {
+            return (
+                (card.enTitle && card.enTitle.toLowerCase().indexOf(searchValue) > -1) || 
+                (card.jpTitle && card.jpTitle.indexOf(searchValue) > -1)
+            );
+        });
+    }
+
+    public onResetFilter(): void {
+        this.cards = [...this.cardMasterList];
     }
 }
